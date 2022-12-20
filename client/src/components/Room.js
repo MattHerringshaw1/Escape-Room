60f6492925f31e14f833db254a44f18504ee641c
Eddie Zaboroskie <Zaboroskie.Edward@gmail.com>
2022-12-20 15:16:02 -0500

    showhideflag

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Inventory from './Popups/Inventory'
 import '../styles/room.css'
 import { connect } from 'react-redux'
+import FlagPuzzle from './FlagPuzzle'
 
 
 
@@ -9,13 +10,11 @@ function Room(props) {
 
     const [doorCode, setDoorCode] = useState(0)
     const [doorOpen, setDoorOpen] = useState(false)
+    const [showFlag, setShowFlag] = useState(false)
 
     // Is the pop-up open or not? 
     const [isOpen, setIsOpen] = useState(false);
 
-    const[leftFlag, setLeftFlag] = useState('')
-    const[centerFlag, setCenterFlag] = useState('')
-    const[rightFlag, setRightFlag] = useState('')
 
     const username = localStorage.getItem('username')
 
@@ -24,18 +23,6 @@ function Room(props) {
         setIsOpen(!isOpen);
     }
     
-    const handleChangeBlue = () =>{
-        setLeftFlag('blue')
-    }
-
-    const handleChangeWhite = () =>{
-        setCenterFlag('white')
-    }
-
-    const handleChangeRed = () =>{
-        setRightFlag('red')
-    }
-
 
     const handleDoorOpen = (doorCode) =>{
         
@@ -46,65 +33,17 @@ function Room(props) {
         }
     }
 
-    const handleCheckColors = () =>{
-        if(leftFlag=='blue' && centerFlag=='white' && rightFlag=='red'){
-            alert('The code is 612')
-        }else{
-            return
-        }
+    const handleShowFlag = () =>{
+        setShowFlag(true)
     }
 
-    
+    const handleHideFlag = () =>{
+        setShowFlag(false)
+    }
 
     return(
         <>
     
-        <div className='door-code'>
-            <h3>Enter Door Code</h3>
-            <input type='text' onChange={(e)=>setDoorCode(e.target.value)}/>
-            <button onClick={()=>handleDoorOpen(doorCode)}>Try Door</button>
-        </div>
-
-
-        <div className='flag-container'>
-            
-            {leftFlag=='' &&(
-            <div className='left-flag'>
-                <button onClick={handleChangeBlue}>Blue</button>
-            </div>
-            )}
-
-            {leftFlag=='blue'&&(
-            <div className='left-flag-blue'>
-            
-            </div>   
-            )}
-
-            {centerFlag=='' &&(
-            <div className='center-flag'>
-                <button onClick={handleChangeWhite}>White</button>
-            </div>   
-            )}
-
-            {centerFlag=='white' &&(
-            <div className='center-flag-white'>
-            </div>   
-            )} 
-
-            {rightFlag=='' &&(
-            <div className='right-flag'>
-                <button onClick={handleChangeRed}>Red</button>
-            </div>
-            )}
-
-            {rightFlag=='red' &&(
-            <div className='right-flag-red'>
-            </div>
-            )}
-            <div><button onClick={handleCheckColors}>Try Colors</button></div>
-            
-        </div>
-
         {doorOpen &&(
         <div className='door-open'>
             <h3>You Escaped!</h3>
@@ -116,6 +55,25 @@ function Room(props) {
             <h3>You're Trapped</h3>
         </div>
         )}
+
+        <div className='door-code'>
+            <h3>Enter Door Code</h3>
+            <input type='text' onChange={(e)=>setDoorCode(e.target.value)}/>
+            <button onClick={()=>handleDoorOpen(doorCode)}>Try Door</button>
+        </div>
+
+        {!showFlag &&(
+        <button onClick={handleShowFlag}>Show Flag Puzzle</button>    
+        )}    
+        
+      
+       {showFlag &&(
+        <>
+        <button onClick={handleHideFlag}>Hide Flag Puzzle</button>
+        <h1>Bonjour!</h1>
+        <FlagPuzzle/>
+        </> 
+       )}
        
 
         <div className='main'>
