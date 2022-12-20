1ffdf3e8a91d3835fcb9a88ef5c57d4bec5bad61
Rob Hackett <108783314+Robhack623@users.noreply.github.com>
2022-12-20 13:16:52 -0500

    drawer opens with screwdriver

--- client/src/components/Room.js
+++ client/src/components/Room.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import Inventory from './Popups/Inventory'
-import '../styles/Room.css'
 import { connect } from 'react-redux'
-
+import Drawer from './Drawer'
+import '../styles/room.css'
 
 
 function Room(props) {
@@ -10,63 +10,65 @@ function Room(props) {
     const [doorCode, setDoorCode] = useState(0)
     const [doorOpen, setDoorOpen] = useState(false)
 
-     const username = localStorage.getItem('username')
+    const username = localStorage.getItem('username')
 
     // Is the pop-up open or not? 
     const [isOpen, setIsOpen] = useState(false);
-    
+
     // function to toggle the pop-up
     const toggleInventory = () => {
         setIsOpen(!isOpen);
     }
-    
 
-    const handleDoorOpen = (doorCode) =>{
-        
-        if (doorCode==612){
+
+    const handleDoorOpen = (doorCode) => {
+
+        if (doorCode == 612) {
             setDoorOpen(true)
-        }else{
+        } else {
             return
         }
     }
 
-
-
-    return(
-        <>
     
-        <div className='door-code'>
-            <h3>Enter Door Code</h3>
-            <input type='text' onChange={(e)=>setDoorCode(e.target.value)}/>
-            <button onClick={()=>handleDoorOpen(doorCode)}>Try Door</button>
-        </div>
+
+
+    return (
+        <>
+            <div className='main'>
+                <h1>main room</h1>
+                <div className='door-code'>
+                    <h3>Enter Door Code</h3>
+                    <input type='text' onChange={(e) => setDoorCode(e.target.value)} />
+                    <button onClick={() => handleDoorOpen(doorCode)}>Try Door</button>
+                </div>
 
 
 
-        {doorOpen &&(
-        <div className='door-open'>
-            <h3>You Escaped!</h3>
-        </div>
-        )}
+                {doorOpen && (
+                    <div className='door-open'>
+                        <h3>You Escaped!</h3>
+                    </div>
+                )}
 
-        {!doorOpen &&(
-        <div className='door-closed'>
-            <h3>You're Trapped</h3>
-        </div>
-        )}
-       
+                {!doorOpen && (
+                    <div className='door-closed'>
+                        <h3>You're Trapped</h3>
+                    </div>
+                )}
 
-        <div className='main'>
-        <h1>main room</h1>
-        <div className='open-inventory' onClick={toggleInventory}>CLICK TO OPEN INVENTORY</div>
-        <div  className='add-screwdriver' onClick={props.setScrewdriver}>{props.hasScrewdriver ? null: <div>CLICK TO ADD SCREWDRIVER TO INVENTORY</div>}</div>
-        <div className='inventory-popup'>{isOpen && <Inventory handleClose={toggleInventory}/>}</div>
-        </div>
- </>
+
+
+                <div className='open-inventory' onClick={toggleInventory}>CLICK TO OPEN INVENTORY</div>
+                <div className='add-screwdriver' onClick={props.setScrewdriver}>{props.hasScrewdriver ? null : <div>CLICK TO ADD SCREWDRIVER TO INVENTORY</div>}</div>
+                <div>{<Drawer />}</div>
+                <div className='inventory-popup'>{isOpen && <Inventory handleClose={toggleInventory} />}</div>
+            </div>
+        </>
     )
 }
-    
-  
+
+
 
 
 const mapStateToProps = (state) => {
@@ -77,7 +79,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        setScrewdriver: () => dispatch({type: 'SET_SCREWDRIVER'})
+        setScrewdriver: () => dispatch({ type: 'SET_SCREWDRIVER' })
     }
 }
 
