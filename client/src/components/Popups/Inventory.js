91e18387b38078bcc70d421a92f8574b3bd05f3a
Rob Hackett <108783314+Robhack623@users.noreply.github.com>
2023-01-06 11:34:57 -0500

    added pics to inventory, fixed some weird css in user info

--- client/src/components/Popups/Inventory.js
+++ client/src/components/Popups/Inventory.js
@@ -5,22 +5,22 @@ import Scissors from '../Inventory/Remote'
 import Key from '../Inventory/Key'
 import MagnifyingGlass from '../Inventory/MagnifyingGlass'
 import Lighter from '../Inventory/Lighter'
-// import '../styles/inventory.css'
+import './inventory.css'
 
 function Inventory(props) {
 
     const username = localStorage.getItem('username')
 
     return (
-        <>
-            <h2>Inventory:</h2>
-            <div className='inv-screwdriver' onClick={props.setScrewdriver}>Slot 1: {props.hasScrewdriver && <Screwdriver/>}</div>
-            <div className='inv-scissors' onClick={props.setScissors}>Slot 2: {props.hasScissors && <Scissors/>}</div>
-            <div className='inv-key' onClick={props.setKey}>Slot 3: {props.hasKey && <Key/>}</div>
-            <div className='inv-magnifyingglass' onClick={props.setMagnifyingGlass}>Slot 4: {props.hasMagnifyingGlass && <MagnifyingGlass/>}</div>
-            <div className='inv-lighter' onClick={props.setLighter}>Slot 5: {props.hasLighter && <Lighter/>}</div>
-            
-        </>
+        <div className='inv-box'>
+            <div className='title'>Inventory</div>
+            <div className='inv-items'>
+                <div className='inv-screwdriver' onClick={props.setScrewdriver}>{props.hasScrewdriver && <Screwdriver />}</div>
+                <div className='inv-scissors' onClick={props.setScissors}>{props.hasScissors && <Scissors />}</div>
+                <div className='inv-key' onClick={props.setKey}>{props.hasKey && <Key />}</div>
+                <div className='inv-magnifyingglass' onClick={props.setMagnifyingGlass}>{props.hasMagnifyingGlass && <MagnifyingGlass />}</div>
+            </div>
+        </div>
     )
 }
 
@@ -36,11 +36,11 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
     return {
-        setScrewdriver: () => dispatch({type: 'DELETE_SCREWDRIVER'}),
-        setScissors: () => dispatch({type: 'DELETE_SCISSORS'}),
-        setKey: () => dispatch({type: 'DELETE_KEY'}),
-        setMagnifyingGlass: () => dispatch({type: 'DELETE_MAGNIFYINGGLASS'}),
-        setLighter: () => dispatch({type: 'DELETE_LIGHTER'})
+        setScrewdriver: () => dispatch({ type: 'DELETE_SCREWDRIVER' }),
+        setScissors: () => dispatch({ type: 'DELETE_SCISSORS' }),
+        setKey: () => dispatch({ type: 'DELETE_KEY' }),
+        setMagnifyingGlass: () => dispatch({ type: 'DELETE_MAGNIFYINGGLASS' }),
+        setLighter: () => dispatch({ type: 'DELETE_LIGHTER' })
     }
 }
 
